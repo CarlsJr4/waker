7621d1416a1151fbb70d7ad99bf35802c3062a65
Carl Dungca <carl.dungca@yahoo.com>
2019-05-17 14:37:30 -0700

    Rendered a static JSX list for FilterList component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+// At first, render static data only!
+
+// Today's goals:
+// 1. Create static filterable list
+// 2. Use map to render the list
+// 3. Refactor into multiple components if needed 
+
+// const taskItems = [
+//   {
+//     id: 1,
+//     body: 'one'
+//   },
+//   {
+//     id: 2,
+//     body: 'two'
+//   },
+//   {
+//     id: 3,
+//     body: 'three'
+//   }
+// ]
+
+function SearchBar() {
   return (
-    <h1>Hello</h1>
-  );
+    <input type="text" placeholder="Search a task..."></input>
+  )
 }
 
-export default App;
+function TaskHolder() {
+  return (
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
+  )
+}
+
+class FilterList extends Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <TaskHolder />
+      </div>
+    );
+  }
+}
+
+export default FilterList;
