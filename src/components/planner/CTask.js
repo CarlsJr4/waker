e8c2b0ee0932f2b9b4cc52a02cc3f41dc1bdbcb2
Carl Dungca <carl.dungca@yahoo.com>
2019-05-27 13:47:24 -0700

    Dropped list items are now drag sources

--- src/components/planner/CTask.js
+++ src/components/planner/CTask.js
@@ -1,12 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { DragSource } from 'react-dnd';
+
+
+const CTaskSpec = {
+    beginDrag(props, monitor, component) {
+        const item = { id: props.id }
+        return item
+    }
+}
+
+function collect(connect, monitor) {
+    return {
+        connectDragSource: connect.dragSource()
+    }
+}
 
 export class CTask extends Component {
     render() {
-        const {id, index, body} = this.props
-        return (
+        const {id, index, body, connectDragSource} = this.props
+        return connectDragSource(
             <li key={id}>{index + 1}. {body}</li>
         )
     }
 }
 
-export default CTask
+export default DragSource("CALENDARTASK", CTaskSpec, collect)(CTask)
+
+// NOTE: This component will be BOTH a drag source and a drop target.
