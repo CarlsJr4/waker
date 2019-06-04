9336c157bff171d91eaac55bb0bc6e52915b7e16
Carl Dungca <carl.dungca@yahoo.com>
2019-06-03 17:58:27 -0700

    Updates

---
+++ precache-manifest.c873ce37c42e4b531e38d5e13593d3b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cac62ee04248fe80b322ec7a2b5d13f2",
+    "url": "/daily-planner/index.html"
+  },
+  {
+    "revision": "8c03994c6194a085f0df",
+    "url": "/daily-planner/static/css/main.da713859.chunk.css"
+  },
+  {
+    "revision": "de3c28276967e8a1c859",
+    "url": "/daily-planner/static/js/2.ffc11e07.chunk.js"
+  },
+  {
+    "revision": "8c03994c6194a085f0df",
+    "url": "/daily-planner/static/js/main.db8e6176.chunk.js"
+  },
+  {
+    "revision": "f6ffd485c2689ab998b8",
+    "url": "/daily-planner/static/js/runtime~main.62d44102.js"
+  }
+]);
